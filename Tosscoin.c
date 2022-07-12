aa373cb0fc729498a34433fd10313d8645319ed4
s1290155 <s1290155@std1dc5>
2022-07-12 16:18:08 +0900

    HoT: initial revision

---
+++ Tosscoin.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main()
+{
+  srand((unsigned int) time(NULL));
+  int cnt1=0, cnt2=0;
+  
+  printf("Tossing a coin...\n");
+
+  for(int i = 1 ; i < 4 ; i++){
+    printf("Round %d: ", i);
+    int res = rand() % 2;
+    if(res == 0){
+      printf("Heads\n");
+      cnt1++;
+    }
+    else{
+      printf("Tails\n");
+      cnt2++;
+    }
+  }
+
+  printf("Heads: %d, Tails: %d\n", cnt1, cnt2);
+
+  return 0;
+}
